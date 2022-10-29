14ed7ab46774e0ba982e1504d05b8b563e993249
Igor Korman <ikorman12@gmail.com>
2022-10-28 23:01:51 -0400

    server working, Heroku not properly working

--- server.js
+++ server.js
@@ -10,11 +10,27 @@ const path= require('path');
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use('/api/notes';
 
 //GET route for homepage
 app.get('/', (req,res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+//GET route for note taking page
+app.get('/notes', (req,res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//GET route for api/notes
+// app.get('/api/notes', (req,res) =>
+// fs.writeFile('')
+
+//POST route for adding notes to notes api
+
+app
+
+
 
 app.listen(PORT, () =>
 console.log(`static asset on port ${PORT}`))

6fd93c656378dae69989a895597842ad7986c1b0
Igor Korman <ikorman12@gmail.com>
2022-10-29 00:08:34 -0400

    post to write new note is working but causing a syntax error in dbv.json

--- server.js
+++ server.js
@@ -12,30 +12,74 @@ const path= require('path');
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // app.use('/api/notes';
 
 //GET route for homepage
-app.get('/', (req,res) =>
+app.get('/', (req,res) => {
 res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
+//Log our request in the terminal
+console.info(`${req.method} request received`);
+});
+
 
 //GET route for note taking page
-app.get('/notes', (req,res) =>
+app.get('/notes', (req,res) => {
 res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+
+console.info(`${req.method} request received`);
+});
 
 
-// GET route for api/notes
+// GET route for all notes
 app.get('/api/notes', (req,res) =>
 {
-    res.status(200).json(notes);
+    return res.status(200).json(notes);
 });
 
 //POST route for adding notes to notes api
+app.post('/api/notes', (req, res) => {
+    // Let the client know that their POST request was received
+    res.json(`${req.method} request received to add a note`);
+    const { title, text} = req.body;
+
+  // Check if there is anything in the response body
+  if (title && text) {
+    const newNote={
+        title,
+        text,
+    };
+    // convert data into string
+    const noteString =JSON.stringify(newNote);
+
+    //write string to a file
+    fs.appendFile(`./db/db.json`,
+    noteString, (err) => 
+    err
+    ? console.error(err)
+    : console.log(
+        `Review for ${newNote.product} has been written to JSON file`
+      )
+    );
+
+    const response = {
+      status: 'success',
+      data: req.body,
+    };
+
+  // Log the response body to the console
+  console.log(response);
+  res.status(201).json(response);
+}else {
+    res.status(500).json(`Error in posting note`);
+}
+  
+});
 
 // app
 
 
 
 app.listen(PORT, () =>
-console.log(`static asset on port ${PORT}`))
+console.log(`App listening at http://localhost:${PORT}🚀`));

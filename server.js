c95dcbc8593a3a23d2b4e3a0c9a1fb49f348a743
Igor Korman <ikorman12@gmail.com>
2022-10-28 22:46:37 -0400

    server is running, got to homepage

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const fs = require('fs');
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const path= require('path');
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));r
 
 //GET route for homepage
 app.get('/', (req,res) =>
-res.sendFile(path.join(__dirname,'/public/index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.listen(PORT, () =>
+console.log(`static asset on port ${PORT}`))
